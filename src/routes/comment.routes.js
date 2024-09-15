1d5397e5c2f2ec3127db025007c11403118d8983
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2024-09-15 20:19:44 +0530

    public

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -9,9 +9,9 @@ import {
 } from "../controllers/comment.controller.js";
 
 const router = Router();
-router.route("/:videoId").get(getVideoComments);
-router.use(verifyJWT);
 
+router.use(verifyJWT);
+router.route("/:videoId").get(getVideoComments);
 router.route("/:videoId").post(addComment);
 router.route("/c/:commentId").delete(deleteComment).put(updateComment);
 
