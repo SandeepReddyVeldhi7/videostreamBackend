d54bbdb8666d5dd44a7484784846034db371e4ef
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2024-09-15 12:46:32 +0530

    error

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://videostream-frontend.vercel.app",
-      "https://videostream-frontend-gsc5w0c9x-sandeepreddys-projects.vercel.app",
-      "https://videostream-frontend-i3je62v37-sandeepreddys-projects.vercel.app",
+    
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
