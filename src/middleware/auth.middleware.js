f9df2b83e52fac968c9a275fa7f2e7d20fea0464
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2024-09-15 14:36:17 +0530

    jwt

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,40 +5,17 @@ import { APIError } from "../utils/APIError.js";
 
 dotenv.config();
 
-const verifyJWT = async (req, res, next) => {
-  try {
-    const token =
-      req.cookies?.accessToken || // Check if token is stored in cookies
-      req.header("Authorization")?.replace("Bearer ", ""); // Or check Authorization header
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    console.log("Received Token:", token); // Add logging for debugging
+  if (token == null) return next(new APIError("Unauthorized request", 401));
 
-    if (!token) {
-      throw new APIError(401, "Unauthorized request");
-    }
-
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log("Decoded Token: ", decodedToken); // Log decoded token to check user details
-
-    const user = await User.findById(decodedToken?._id).select(
-      "-password -refreshToken"
-    );
-    if (!user) {
-      throw new APIError(401, "Invalid Access Token");
-    }
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return next(new APIError("Unauthorized request", 401));
     req.user = user;
     next();
-  } catch (error) {
-    console.error("Error in verifyJWT middleware:", error); // Log the error
-    if (error.name === "TokenExpiredError") {
-      return next(new APIError(401, "Access token expired"));
-    } else if (error.name === "JsonWebTokenError") {
-      return next(new APIError(401, "Invalid access token"));
-    } else {
-      return next(new APIError(401, "Unauthorized request"));
-    }
-  }
+  });
 };
 
 
