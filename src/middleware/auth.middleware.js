138958a70754cc4f3534a12a33f93182b001e89e
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2024-09-15 20:26:00 +0530

    public

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,33 +6,30 @@ import { APIError } from "../utils/APIError.js";
 dotenv.config();
 
 const verifyJWT = async (req, _, next) => {
-  const authHeader = req.header("Authorization");
-  const token =
-    authHeader && authHeader.startsWith("Bearer ")
-      ? authHeader.split(" ")[1]
-      : null;
-
-  console.log("123456", token);
-
-  if (!token) {
-    throw new APIError(401, "unauthorization");
-  }
-
   try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    console.Consolelog("token",token)
+    if (!token) {
+      throw new APIError(401, "Unauthorized Request");
+    }
+
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken?._id).select(
-      "password -refreshToken"
+      "-password -refreshToken"
     );
+
     if (!user) {
-      throw new APIError(401, "unauthorized");
+      throw new APIError(401, "Invalid Access Token");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new APIError(401, error?.message || "Invalid access token");
-  }
+    throw new APIError(401, error?.message || "Invalid Access Token");
+  } 
 };
 
 
